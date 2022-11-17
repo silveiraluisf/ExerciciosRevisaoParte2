5817f39bfd7d80a8e65d7b028b88caa4e50bd687
silveiraluisf <silveiraluis88@gmail.com>
2022-11-17 13:42:22 -0300

    Add project files.

---
+++ ExerciciosRevisaoParte2/Client.cs
@@ -0,0 +1,43 @@
+﻿namespace Cliente
+{
+    public class Client
+    {
+        private string _Name;
+        private long _CPF;
+        private DateTime _BornDate;
+        private float _MonthlyIncome;
+        private char _MaritalStatus;
+        private int _NumberOfDependents;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+        public long CPF
+        {
+            get { return _CPF; }
+            set { _CPF = value; }
+        }
+        public DateTime BornDate
+        {
+            get { return _BornDate; }
+            set { _BornDate = value; }
+        }
+        public float MonthlyIncome
+        {
+            get { return _MonthlyIncome; }
+            set { _MonthlyIncome = value; }
+        }
+        public char MaritalStatus
+        {
+            get { return _MaritalStatus; }
+            set { _MaritalStatus = value; }
+        }
+        public int NumberOfDependents
+        {
+            get { return _NumberOfDependents; }
+            set { _NumberOfDependents = value; }
+        }
+    }
+}
