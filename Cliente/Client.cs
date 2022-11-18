1054ba71b7b1b383edada38e94060006c1dab3f6
silveiraluisf <silveiraluis88@gmail.com>
2022-11-17 21:05:05 -0300

    Divisão das classe Client e Validator

---
+++ Cliente/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    public class Client
+    {
+        //private string Name;
+        //private long CPF;
+        //private DateTime BornDate;
+        //private float MonthlyIncome;
+        //private char MaritalStatus;
+        //private int NumberOfDependents;
+
+        public string Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public long CPF
+        {
+            get { return CPF; }
+            set { CPF = value; }
+        }
+        public DateTime BornDate
+        {
+            get { return BornDate; }
+            set { BornDate = value; }
+        }
+        public float MonthlyIncome
+        {
+            get { return MonthlyIncome; }
+            set { MonthlyIncome = value; }
+        }
+        public char MaritalStatus
+        {
+            get { return MaritalStatus; }
+            set { MaritalStatus = value; }
+        }
+        public int NumberOfDependents
+        {
+            get { return NumberOfDependents; }
+            set { NumberOfDependents = value; }
+        }
+
+    }
+}
