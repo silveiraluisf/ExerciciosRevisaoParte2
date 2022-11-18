4aff2b79dbb60be40f3dd03ae00ae1123273f214
silveiraluisf <silveiraluis88@gmail.com>
2022-11-18 19:34:22 -0300

    A função de inserir nome do Cliente agora funciona dentro da arquitetura MVC

--- Cliente/controle/ClientController.cs
+++ Cliente/controle/ClientController.cs
@@ -2,17 +2,17 @@
 {
     internal class ClientController
     {
-        public void NameValidate(entidade.Client C)
+        public void NameValidate(entidade.Client C, limite.Interface A)
         {
-            C.Name = Console.ReadLine();
-            string name = C.Name;
+            string name = A.GetName();
+            //string name = C.Name;
             switch (name.Length)
             {
                 case 0:
-                    NameValidate(C);
+                    NameValidate(C, A);
                     break;
                 case < 5:                    
-                    NameValidate(C);
+                    NameValidate(C, A);
                     break;
                 case >= 5:
                     C.Name = name;
@@ -20,128 +20,128 @@
             }
         }
 
-        public void CPFValidate(entidade.Client C)
-        {
-            string inputCPF = Console.ReadLine();
-            long outputCPF;
-            switch (inputCPF.Length)
-            {
-                case 11:
-                    bool parseSuccess = long.TryParse(inputCPF, out outputCPF);
-                    if (parseSuccess)
-                    {
-                        //CPF = Convert.ToInt64(inputCPF);
-                        C.CPF = outputCPF;
-                    }
-                    else
-                    {
-                        CPFValidate(C);
-                    }
-                    break;
-                default:
-                    CPFValidate(C);
-                    break;
-            }
-        }
+        //public void CPFValidate(entidade.Client C)
+        //{
+        //    string inputCPF = Console.ReadLine();
+        //    long outputCPF;
+        //    switch (inputCPF.Length)
+        //    {
+        //        case 11:
+        //            bool parseSuccess = long.TryParse(inputCPF, out outputCPF);
+        //            if (parseSuccess)
+        //            {
+        //                //CPF = Convert.ToInt64(inputCPF);
+        //                C.CPF = outputCPF;
+        //            }
+        //            else
+        //            {
+        //                CPFValidate(C);
+        //            }
+        //            break;
+        //        default:
+        //            CPFValidate(C);
+        //            break;
+        //    }
+        //}
 
-        public void BirthDateValidate(entidade.Client C)
-        {
-            string inputDate = Console.ReadLine();
-            DateTime outputDate;
-            DateTime now = DateTime.Now;
-            TimeSpan eighteenYears = new TimeSpan(6574, 0, 0, 0);
-            bool parseSuccess = DateTime.TryParse(inputDate, out outputDate);
-            if (parseSuccess)
-            {
-                C.BornDate = Convert.ToDateTime(outputDate);
-                TimeInterval timeInterval = new(outputDate, now);
-                if (timeInterval.Duration > eighteenYears)
-                {
-                    C.BornDate = outputDate;
-                }
-                else
-                {
-                    BirthDateValidate(C);
-                }
-            }
-            else
-            {
-                BirthDateValidate(C);
-            }
+        //public void birthdatevalidate(entidade.client c)
+        //{
+        //    string inputdate = console.readline();
+        //    datetime outputdate;
+        //    datetime now = datetime.now;
+        //    timespan eighteenyears = new timespan(6574, 0, 0, 0);
+        //    bool parsesuccess = datetime.tryparse(inputdate, out outputdate);
+        //    if (parsesuccess)
+        //    {
+        //        c.borndate = convert.todatetime(outputdate);
+        //        timeinterval timeinterval = new(outputdate, now);
+        //        if (timeinterval.duration > eighteenyears)
+        //        {
+        //            c.borndate = outputdate;
+        //        }
+        //        else
+        //        {
+        //            birthdatevalidate(c);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        birthdatevalidate(c);
+        //    }
 
-        }
+        //}
 
-        public void IncomeValidate(entidade.Client C)
-        {
-            string inputIncome = Console.ReadLine();
-            float outputIncome;
-            bool parseSuccess = float.TryParse(inputIncome, out outputIncome);
-            if (parseSuccess)
-            {
-                C.MonthlyIncome = (float)Convert.ToDouble(outputIncome);
-            }
-            else
-            {
-                IncomeValidate(C);
-            }
-        }
-        public void StatusValidate(entidade.Client C)
-        {
-            string inputStatus = Console.ReadLine();
-            char outputStatus;
-            bool parseSuccess = char.TryParse(inputStatus, out outputStatus);
-            if (parseSuccess)
-            {
-                Convert.ToChar(outputStatus);
-                switch (char.ToUpper(outputStatus))
-                {
-                    case 'C':
-                        C.MaritalStatus = 'C';
-                        break;
-                    case 'S':
-                        C.MaritalStatus = 'S';
-                        break;
-                    case 'V':
-                        C.MaritalStatus = 'V';
-                        break;
-                    case 'D':
-                        C.MaritalStatus = 'D';
-                        break;
-                    default:
-                        StatusValidate(C);
-                        break;
-                }
-            }
-            else
-            {
-                StatusValidate(C);
-            }
-        }
+        //public void IncomeValidate(entidade.Client C)
+        //{
+        //    string inputIncome = Console.ReadLine();
+        //    float outputIncome;
+        //    bool parseSuccess = float.TryParse(inputIncome, out outputIncome);
+        //    if (parseSuccess)
+        //    {
+        //        C.MonthlyIncome = (float)Convert.ToDouble(outputIncome);
+        //    }
+        //    else
+        //    {
+        //        IncomeValidate(C);
+        //    }
+        //}
+        //public void StatusValidate(entidade.Client C)
+        //{
+        //    string inputStatus = Console.ReadLine();
+        //    char outputStatus;
+        //    bool parseSuccess = char.TryParse(inputStatus, out outputStatus);
+        //    if (parseSuccess)
+        //    {
+        //        Convert.ToChar(outputStatus);
+        //        switch (char.ToUpper(outputStatus))
+        //        {
+        //            case 'C':
+        //                C.MaritalStatus = 'C';
+        //                break;
+        //            case 'S':
+        //                C.MaritalStatus = 'S';
+        //                break;
+        //            case 'V':
+        //                C.MaritalStatus = 'V';
+        //                break;
+        //            case 'D':
+        //                C.MaritalStatus = 'D';
+        //                break;
+        //            default:
+        //                StatusValidate(C);
+        //                break;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        StatusValidate(C);
+        //    }
+        //}
 
-        public void DependentsValidate(entidade.Client C)
-        {
-            string inputDependents = Console.ReadLine();
-            int outputDependents;
-            bool parseSuccess = int.TryParse(inputDependents, out outputDependents);
-            if (parseSuccess)
-            {
-                C.NumberOfDependents = Convert.ToInt32(outputDependents);
-                int n = C.NumberOfDependents;
-                switch (n)
-                {
-                    case >= 0 and <= 10:
-                        C.NumberOfDependents = outputDependents;
-                        break;
-                    default:
-                        DependentsValidate(C);
-                        break;
-                }
-            }
-            else
-            {
-                DependentsValidate(C);
-            }
+        //public void DependentsValidate(entidade.Client C)
+        //{
+        //    string inputDependents = Console.ReadLine();
+        //    int outputDependents;
+        //    bool parseSuccess = int.TryParse(inputDependents, out outputDependents);
+        //    if (parseSuccess)
+        //    {
+        //        C.NumberOfDependents = Convert.ToInt32(outputDependents);
+        //        int n = C.NumberOfDependents;
+        //        switch (n)
+        //        {
+        //            case >= 0 and <= 10:
+        //                C.NumberOfDependents = outputDependents;
+        //                break;
+        //            default:
+        //                DependentsValidate(C);
+        //                break;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        DependentsValidate(C);
+        //    }
         }
     }
 }
-}
+
